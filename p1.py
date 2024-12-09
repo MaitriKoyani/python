ed18ea522cbad19ad718ff528c81a7a422c1dfb9
MaitriKoyani <142216828+MaitriKoyani@users.noreply.github.com>
2024-12-09 11:49:26 +0530

    Delete p1.py

--- p1.py
+++
@@ -1,5 +0,0 @@
-# Exercise 1: Calculate the multiplication and sum of two numbers
-a=int(input('Enter num1:'))
-b=int(input('Enter num2:'))
-print(f'Multplication of {a} and {b} is {a*b}')
-print(f'Multplication of {a} and {b} is {a+b}')
